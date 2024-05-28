5a37beb55ea02ed5d24d24374c45620d9bc33ff3
PlainOldMoose <luquevdm@gmail.com>
2024-05-28 14:44:41 +0100

    initial commit

---
+++ src/main/java/me/plainoldmoose/wardrobe/Wardrobe.java
@@ -0,0 +1,17 @@
+package me.plainoldmoose.wardrobe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Wardrobe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
